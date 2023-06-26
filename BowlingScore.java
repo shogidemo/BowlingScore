d8dbc70350f6c8efa7e7bfbcb790d47a87525b30
SATO Sho <shogidemo@gmail.com>
2023-06-26 13:53:21 +0900

    全部のテストケースをパスした！

--- BowlingScore.java
+++ BowlingScore.java
@@ -2,79 +2,99 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BowlingScore {
-    public static final int NUM_FRAMES = 10;
+    public static final int MAX_FRAMES = 10;
     public static final int MAX_PINS = 10;
-    private final int[] rolls = new int[21];
-    private final int[] frameScores = new int[NUM_FRAMES];
-    private int currentRoll = 0;
-    private int currentFrame = 1;
+    public static final int MAX_THROWS = 21;
+    private final int[] pinsDownList = new int[MAX_THROWS];
+    private final int[] frameScores = new int[MAX_FRAMES];
+    private int currentThrowIndex = 0;
+    private int currentFrameIndex = 0;
 
-    public void roll(int pins) {
+    public void throwBall(int pins) {
     	
     	//1投で倒せるピンの数は1～10本
-        if (pins < 0 || pins > 10) {
+        if (pins < 0 || pins > MAX_PINS) {
             throw new IllegalArgumentException("Invalid number of pins: " + pins);
         }
         
         //1～9フレーム目では、2投の合計が10ピン以内であること
-        if (currentFrame < 10 && 
-        	currentRoll % 2 == 1 && 
-        	rolls[currentRoll - 1] + pins > 10) {
-            
-        	throw new IllegalArgumentException("In one frame, the total number of pins cannot exceed 10.");
-        }
-
-        //10フレーム目では、3投の合計が20ピン以内であること
-        if (currentFrame == 10 && 
-        	currentRoll % 2 == 1  && 
-        	rolls[currentRoll - 1] + rolls[currentRoll - 2] + pins > 20) {
+        if (currentFrameIndex < MAX_FRAMES - 1 && 
+        	currentThrowIndex % 2 == 1 && 
+        	pinsDownList[currentThrowIndex - 1] + pins > 10) {
             
         	throw new IllegalArgumentException("In one frame, the total number of pins cannot exceed 10.");
         }
        
-        rolls[currentRoll++] = pins;
-   
-           //1～9フレーム目でストライクだった場合は2投目をスキップする
-        if (pins == 10 &&
-        	currentRoll % 2 == 1 && 
-        	currentFrame < 10) {
+        pinsDownList[currentThrowIndex] = pins;
+        
+        //スコア計算
+        calculateScores();
+        
+        //1～9フレーム目でストライクだった場合は2投目をスキップする
+        if (currentFrameIndex < MAX_FRAMES - 1 &&
+        	isStrike(currentThrowIndex)) {
            
-        	currentRoll++;
-        }
-               
-        if (currentRoll % 2 == 0) {
-            currentFrame++;
+        	currentThrowIndex++;
         }
         
-        calculateScores();
+        //1～9フレーム目で2投目の場合は次のフレームへ
+        if (currentFrameIndex < MAX_FRAMES - 1) {
+        	if (currentThrowIndex % 2 == 1) {
+        		currentFrameIndex++;
+            }
+        }
+        
+        currentThrowIndex++;
     }
 
     void calculateScores() {
         int rollIndex = 0;
-        for (int frame = 0; frame < NUM_FRAMES; frame++) {
+        for (int frame = 0; frame <= currentFrameIndex; frame++) {
             if (isStrike(rollIndex)) {
-                frameScores[frame] = MAX_PINS + rolls[rollIndex + 1] + rolls[rollIndex + 2];
-                rollIndex++;
+                if (frame == MAX_FRAMES - 1) {
+                	frameScores[frame] = MAX_PINS + pinsDownList[rollIndex + 1] + pinsDownList[rollIndex + 2];
+                } else {
+                	if(isStrike(rollIndex + 2)) {
+                		//2連続ストライク（ダブル）だった場合
+                		if (frame == MAX_FRAMES - 2) {
+                			frameScores[frame] = MAX_PINS + pinsDownList[rollIndex + 2] + pinsDownList[rollIndex + 3];
+                		} else {
+                			frameScores[frame] = MAX_PINS + pinsDownList[rollIndex + 2] + pinsDownList[rollIndex + 4];                			
+                		}
+                	} else {
+                		frameScores[frame] = MAX_PINS + pinsDownList[rollIndex + 2] + pinsDownList[rollIndex + 3];
+                	}                	
+                }
             } else if (isSpare(rollIndex)) {
-                frameScores[frame] = MAX_PINS + rolls[rollIndex + 2];
-                rollIndex += 2;
+                frameScores[frame] = MAX_PINS + pinsDownList[rollIndex + 2];
             } else {
-                frameScores[frame] = rolls[rollIndex] + rolls[rollIndex + 1];
-                rollIndex += 2;
+                frameScores[frame] = pinsDownList[rollIndex] + pinsDownList[rollIndex + 1];
             }
+            
+            rollIndex += 2;
         }
     }
 
-    private boolean isStrike(int rollIndex) {
-        return rolls[rollIndex] == MAX_PINS;
+    private boolean isStrike(int throwIndex) {
+        //フレームの2投目ならストライクではない
+    	if (throwIndex % 2 == 1) {
+    		return false;
+    	}
+    	
+    	//倒したピンの数が10ピンでなければストライクではない
+    	if (pinsDownList[throwIndex] != 10) {
+    		return false;
+    	}
+    	
+    	return true;
     }
 
-    private boolean isSpare(int rollIndex) {
-        return rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS;
+    private boolean isSpare(int throwIndex) {    	
+        return pinsDownList[throwIndex] + pinsDownList[throwIndex + 1] == MAX_PINS;
     }
 
-    public int getFrameScore(int frame) {
-        return frameScores[frame];
+    public int getFrameScore(int frameIndex) {
+        return frameScores[frameIndex];
     }
 
     /**
@@ -82,31 +102,49 @@ public class BowlingScore {
      * @return ゲームが終了していればtrue、そうでなければfalse
      */
     public boolean isGameOver() {
-        // 10フレーム終了か、または投球回数が21回以上ならゲーム終了
-        return currentFrame >= 10 || currentRoll >= 21;
+    	
+    	//最大投球回数以上投げているなら終了している
+    	if (currentThrowIndex >= MAX_THROWS - 1) {
+    		return true;
+    	}
+    	
+    	//最終フレームに達していないなら終了していない
+    	if (currentFrameIndex < MAX_FRAMES - 1) {
+    		return false;
+    	}
+    	
+    	//以降は最終フレーム目を前提とした判断ロジック
+    	
+    	//2投目まで投げ終わっている（=20投目以降である）、かつ1投目と2投目の合計が10本未満なら終了している
+    	if (currentThrowIndex >= MAX_THROWS - 2 && 
+    		pinsDownList[19] + pinsDownList[20] < MAX_PINS) {
+    		return true;
+    	}
+    	                      
+    	return false;
     }
     
-    public void printScores() {
-        for (int i = 0; i < NUM_FRAMES; i++) {
-            if (frameScores[i] != -1) {
-                System.out.printf("Frame %d: %d\n", i + 1, frameScores[i]);
-            } else {
-                System.out.printf("Frame %d: \n", i + 1);
-            }
-        }
-        System.out.printf("Total Score: %d\n", getTotalScore());
-    }
-
     public int getTotalScore() {
         return Arrays.stream(frameScores).sum();
     }
 
-    public int getRoll(int rollIndex) {
-        return rolls[rollIndex];
+    public int getPinsDown(int throwIndex) {
+        return pinsDownList[throwIndex];
+    }
+    
+    public void printScores() {
+    	for (int i = 0; i <= currentFrameIndex; i++) {
+    		if (frameScores[i] != -1) {
+    			System.out.printf("Frame %d: %d\n", i + 1, frameScores[i]);
+    		} else {
+    			System.out.printf("Frame %d: \n", i + 1);
+    		}
+    	}
+    	System.out.printf("Total Score: %d\n", getTotalScore());
     }
 
     public static void main(String[] args) {
-        BowlingScore game = new BowlingScore();
+        BowlingScore game;
         Scanner scanner = new Scanner(System.in);
         while (true) {
             game = new BowlingScore();
@@ -121,11 +159,12 @@ public class BowlingScore {
     }
 
 	private static void playBowling(BowlingScore game, Scanner scanner) {
-		for (int i = 0; i < 21; i++) {
+		for (int i = 0; i < MAX_THROWS; i++) {
 		    try {
-		        System.out.printf("Frame %d, roll %d:\n", (i / 2) + 1, (i % 2) + 1);
-		        game.roll(scanner.nextInt());
-		        game.printScores();
+		    	System.out.printf("Frame %d, roll %d:\n", game.currentFrameIndex + 1, game.currentThrowIndex % 2 + 1);
+		        game.throwBall(scanner.nextInt());
+		        game.printScores();		 
+		        System.out.printf("───────────\n");
 		    } catch (IllegalArgumentException e) {
 		        System.out.println(e.getMessage());
 		        i--;
--- BowlingScoreTest.java
+++ BowlingScoreTest.java
@@ -15,13 +15,13 @@ public class BowlingScoreTest {
 
     @Test
     public void 一投で11ピン以上倒す入力があった場合に例外が発生する() {
-        assertThrows(IllegalArgumentException.class, () -> game.roll(11));
+        assertThrows(IllegalArgumentException.class, () -> game.throwBall(11));
     }
     
     @Test
     public void 一つのフレームで合計ピンが11以上になる場合に例外が発生する() {
-        game.roll(6);
-        assertThrows(IllegalArgumentException.class, () -> game.roll(5));
+        game.throwBall(6);
+        assertThrows(IllegalArgumentException.class, () -> game.throwBall(5));
     }
 	
     @Test
@@ -39,11 +39,11 @@ public class BowlingScoreTest {
     @Test
     public void 全ての投球でスペアを取った場合() {
         for (int i = 0; i < 10; i++) {
-            game.roll(5);
-            game.roll(5);
+            game.throwBall(5);
+            game.throwBall(5);
         }
         // 最終フレームでスペアを取った場合、1回追加で投げられる
-        game.roll(5);
+        game.throwBall(5);
         assertEquals(150, game.getTotalScore());
     }
 
@@ -55,44 +55,53 @@ public class BowlingScoreTest {
    
     @Test
     public void フレームの最初の投球で全てのピンを倒さなかった場合_そのフレームのスコアは2回の投球で倒したピンの数となる() {
-    	game.roll(5);
-    	game.roll(3);
-        rollMany(0, 18);  // 2-10フレーム目は全て0ピン
+    	game.throwBall(5);
+    	game.throwBall(3);
+        rollMany(18, 0);  // 2-10フレーム目は全て0ピン
         assertEquals(8, game.getFrameScore(0));
     }
     
     @Test
     public void フレームの1投目で全てのピンを倒した場合_そのフレームの得点は10点に加え次の2回の投球で倒したピンの数が加算される() {
-        game.roll(10);  // ストライク
-        game.roll(3);
-        game.roll(4);
-        rollMany(0, 16);  // 残りのフレームは全て0ピン
+        game.throwBall(10);  // ストライク
+        game.throwBall(3);
+        game.throwBall(4);
+        rollMany(16, 0);  // 残りのフレームは全て0ピン
         assertEquals(17, game.getFrameScore(0));
     }
 
     @Test
     public void フレームの2投目で全てのピンを倒した場合_そのフレームの得点は10点に加え次の1投のピンの数が加算される() {
-        game.roll(5);
-        game.roll(5);  // スペア
-        game.roll(3);
-        rollMany(0, 17);  // 残りのフレームは全て0ピン
+        game.throwBall(5);
+        game.throwBall(5);  // スペア
+        game.throwBall(3);
+        rollMany(17, 0);  // 残りのフレームは全て0ピン
         assertEquals(13, game.getFrameScore(0));
     }
 
     @Test
-    public void 最終フレームでストライクまたはスペアを投じた場合_追加の投球で倒したピンの数も10フレーム目の得点に加算される() {
-        rollMany(0, 18);  // 1-9フレーム目は全て0ピン
-        game.roll(10);  // ストライク
-        game.roll(3);
-        game.roll(4);
+    public void 最終フレームでストライクを投じた場合_追加の投球で倒したピンの数も10フレーム目の得点に加算される() {
+        rollMany(18, 0);  // 1-9フレーム目は全て0ピン
+        game.throwBall(10);  // ストライク
+        game.throwBall(3);
+        game.throwBall(4);
         assertEquals(17, game.getFrameScore(9));
     }
 
     @Test
+    public void 最終フレームでスペアを投じた場合_追加の投球で倒したピンの数も10フレーム目の得点に加算される() {
+        rollMany(18, 0);  // 1-9フレーム目は全て0ピン
+        game.throwBall(6);
+        game.throwBall(4); //スペア
+        game.throwBall(3);
+        assertEquals(13, game.getFrameScore(9));
+    }
+    
+    @Test
     public void 最終フレームで2投なげて全ピン倒せなかった場合にゲームが終了する() {
         rollMany(18, 0);
-        game.roll(4);
-        game.roll(5);
+        game.throwBall(4);
+        game.throwBall(5);
         Assertions.assertTrue(game.isGameOver());
     }
     
@@ -105,21 +114,21 @@ public class BowlingScoreTest {
     @Test
     public void 入力されるスコアの値は0から10の範囲でなければならない() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            game.roll(11);
+            game.throwBall(11);
         });
     }
 
     @Test
     public void フレームの1投目と2投目で合計で倒せるピンの数も最大10ピンでなければならない() {
-        game.roll(5);
+        game.throwBall(5);
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            game.roll(6);
+            game.throwBall(6);
         });
     }
     
     private void rollMany(int numberOfRolls, int pins) {
         for (int i = 0; i < numberOfRolls; i++) {
-            game.roll(pins);
+            game.throwBall(pins);
         }
     }
 }
